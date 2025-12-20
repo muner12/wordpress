31d88a17d0fe18e16cccf42ae106f845ad652bf0
muner12 <bakhtmuner06@gmail.com>
2025-12-21 01:53:28 +0500

    new changes

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,24 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'test' );
+define( 'DB_NAME', 'u300605004_Yyoa4' );
 
 /** Database username */
-define( 'DB_USER', 'root1' );
+define( 'DB_USER', 'u300605004_3Tg8C' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '@Store12' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+/**#@+
 
 /**#@+
  * Authentication unique keys and salts.
