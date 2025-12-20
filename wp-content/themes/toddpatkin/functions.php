27205a373a22fdb51b6aa083e72d2b97cff32ec3
muner12 <bakhtmuner06@gmail.com>
2025-12-21 02:42:06 +0500

    new chnages

--- wp-content/themes/toddpatkin/functions.php
+++ wp-content/themes/toddpatkin/functions.php
@@ -15,3 +15,111 @@ if ( ! defined( '_S_VERSION' ) ) {
 require_once get_template_directory() . '/inc/theme-setup.php';
 // require_once get_template_directory() . '/inc/enqueue.php';
 // require_once get_template_directory() . '/inc/custom-post-types.php';
+
+/**
+ * Create required pages on theme activation
+ */
+function toddpatkin_create_required_pages() {
+    $pages = array(
+        array( 'slug' => 'expertise', 'title' => 'Expertise', 'template' => 'templates/template-expertise.php' ),
+        array( 'slug' => 'about-book', 'title' => 'About Book', 'template' => 'templates/template-about-book.php' ),
+        array( 'slug' => 'about-author', 'title' => 'About Author', 'template' => 'templates/template-about-author.php' ),
+        array( 'slug' => 'course', 'title' => 'Course', 'template' => 'templates/template-course.php' ),
+        array( 'slug' => 'podcast', 'title' => 'Podcast', 'template' => 'templates/template-podcast.php' ),
+        array( 'slug' => 'blog', 'title' => 'Blog', 'template' => 'templates/template-blog.php' ),
+        array( 'slug' => 'preview-book', 'title' => 'Preview Book', 'template' => 'templates/template-preview-book.php' ),
+    );
+    
+    foreach ( $pages as $page_data ) {
+        $page = get_page_by_path( $page_data['slug'] );
+        
+        if ( ! $page ) {
+            $new_page = array(
+                'post_title'    => $page_data['title'],
+                'post_name'     => $page_data['slug'],
+                'post_status'   => 'publish',
+                'post_type'     => 'page',
+                'post_content'  => '',
+                'post_author'   => 1,
+            );
+            
+            $page_id = wp_insert_post( $new_page, true );
+            
+            if ( ! is_wp_error( $page_id ) && $page_id && ! empty( $page_data['template'] ) ) {
+                update_post_meta( $page_id, '_wp_page_template', $page_data['template'] );
+            }
+        } else {
+            // Update template if page exists
+            if ( ! empty( $page_data['template'] ) ) {
+                $current_template = get_page_template_slug( $page->ID );
+                if ( $current_template !== $page_data['template'] ) {
+                    update_post_meta( $page->ID, '_wp_page_template', $page_data['template'] );
+                }
+            }
+        }
+    }
+    
+    flush_rewrite_rules();
+}
+
+// Create pages on theme activation
+add_action( 'after_switch_theme', 'toddpatkin_create_required_pages' );
+
+// Also create pages when admin visits (for existing installations)
+add_action( 'admin_init', 'toddpatkin_create_required_pages' );
+
+/**
+ * Handle 404 errors - create page if it's one of our required pages
+ */
+add_action( 'template_redirect', 'toddpatkin_handle_404_create_page', 1 );
+function toddpatkin_handle_404_create_page() {
+    if ( ! is_404() ) {
+        return;
+    }
+    
+    if ( ! isset( $_SERVER['REQUEST_URI'] ) ) {
+        return;
+    }
+    
+    $request_uri = trim( $_SERVER['REQUEST_URI'], '/' );
+    $path_parts = array_filter( explode( '/', $request_uri ) );
+    $slug = end( $path_parts );
+    $slug = strtok( $slug, '?' );
+    
+    $page_templates = array(
+        'expertise' => array( 'title' => 'Expertise', 'template' => 'templates/template-expertise.php' ),
+        'about-book' => array( 'title' => 'About Book', 'template' => 'templates/template-about-book.php' ),
+        'about-author' => array( 'title' => 'About Author', 'template' => 'templates/template-about-author.php' ),
+        'course' => array( 'title' => 'Course', 'template' => 'templates/template-course.php' ),
+        'podcast' => array( 'title' => 'Podcast', 'template' => 'templates/template-podcast.php' ),
+        'blog' => array( 'title' => 'Blog', 'template' => 'templates/template-blog.php' ),
+        'preview-book' => array( 'title' => 'Preview Book', 'template' => 'templates/template-preview-book.php' ),
+    );
+    
+    if ( ! empty( $slug ) && isset( $page_templates[ $slug ] ) ) {
+        $page_data = array(
+            'post_title'    => $page_templates[ $slug ]['title'],
+            'post_name'     => $slug,
+            'post_status'   => 'publish',
+            'post_type'     => 'page',
+            'post_content'  => '',
+            'post_author'   => 1,
+        );
+        
+        $page_id = wp_insert_post( $page_data, true );
+        
+        if ( ! is_wp_error( $page_id ) && $page_id ) {
+            if ( ! empty( $page_templates[ $slug ]['template'] ) ) {
+                update_post_meta( $page_id, '_wp_page_template', $page_templates[ $slug ]['template'] );
+            }
+            
+            flush_rewrite_rules( false );
+            
+            $permalink = get_permalink( $page_id );
+            if ( $permalink ) {
+                wp_redirect( $permalink, 302 );
+                exit;
+            }
+        }
+    }
+}
